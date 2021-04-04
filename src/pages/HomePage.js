a840a19e8703a645fd98ef920e0b97898bce9752
Lawrence O. Caulker <locaulker@gmail.com>
2021-04-04 05:53:49 -0400

    implemented react-router setup

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,5 +1,6 @@
-import React from 'react'
-import { FeaturedProducts, Hero, Services, Contact } from '../components'
+import React from "react"
+import { FeaturedProducts, Hero, Services, Contact } from "../components"
+
 const HomePage = () => {
   return <h4>home page</h4>
 }
