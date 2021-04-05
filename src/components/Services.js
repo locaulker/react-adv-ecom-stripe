10ddc57570d70fff8fb2d7216d501c18f38f840d
Lawrence <locaulker@gmail.com>
2021-04-05 07:04:11 -0400

    added services section in homepage

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import styled from 'styled-components'
-import { services } from '../utils/constants'
+import { IdealBankElement } from "@stripe/react-stripe-js"
+import React from "react"
+import styled from "styled-components"
+import { services } from "../utils/constants"
 
 const Services = () => {
-  return <h4>services </h4>
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <article className="header">
+          <h3>
+            Custom Furniture <br /> <em>- built only for you</em>
+          </h3>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus
+            illum dolores possimus harum quod reprehenderit voluptates ullam
+            explicabo saepe dolore!
+          </p>
+        </article>
+        <div className="services-center">
+          {services.map(service => {
+            const { id, icon, title, text } = service
+            return (
+              <article key={id} className="service">
+                <span className="icon">{icon} </span>
+                <h4>{title}</h4>
+                <p>{text}</p>
+              </article>
+            )
+          })}
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.section`
