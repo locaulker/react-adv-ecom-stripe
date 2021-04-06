8544d024cab5165cfbce80e62207be734e2b9882
Lawrence O. Caulker <locaulker@gmail.com>
2021-04-05 22:25:53 -0400

    resume L303 @ 4:40

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -10,11 +10,15 @@ import {
   GET_PRODUCTS_ERROR,
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR
+  GET_SINGLE_PRODUCT_ERROR,
 } from "../actions"
 
 const initialState = {
-  isSidebarOpen: false
+  isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  featured_products: [],
 }
 
 const ProductsContext = React.createContext()
@@ -29,13 +33,25 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE })
   }
 
+  // get products
+  const fetchProducts = async url => {
+    dispatch({ type: GET_PRODUCTS_BEGIN })
+
+    const response = await axios.get(url)
+    console.log(response)
+  }
+
+  useEffect(() => {
+    fetchProducts(url)
+  }, [])
+
   return (
     <ProductsContext.Provider value={{ ...state, openSidebar, closeSidebar }}>
       {children}
     </ProductsContext.Provider>
   )
 }
-// make sure use
+// make sure you use
 export const useProductsContext = () => {
   return useContext(ProductsContext)
 }
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -6,7 +6,7 @@ import {
   GET_PRODUCTS_ERROR,
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR
+  GET_SINGLE_PRODUCT_ERROR,
 } from "../actions"
 
 const products_reducer = (state, action) => {
@@ -15,6 +15,10 @@ const products_reducer = (state, action) => {
   }
   if (action.type === SIDEBAR_CLOSE) {
     return { ...state, isSidebarOpen: false }
+  }
+
+  if (action.type === GET_PRODUCTS_BEGIN) {
+    return { ...state, products_loading: true }
   }
 
   throw new Error(`No Matching "${action.type}" - action type`)
