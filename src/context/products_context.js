c81bf26c115315facc7855c2ba6eb6da6a635131
Lawrence O. Caulker <locaulker@gmail.com>
2021-04-06 10:09:39 -0400

    added single product functionality

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -10,7 +10,7 @@ import {
   GET_PRODUCTS_ERROR,
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR
+  GET_SINGLE_PRODUCT_ERROR,
 } from "../actions"
 
 // initial states
@@ -19,7 +19,10 @@ const initialState = {
   products_loading: false,
   products_error: false,
   products: [],
-  featured_products: []
+  featured_products: [],
+  single_product_loading: false,
+  single_product_error: false,
+  single_product: {},
 }
 
 const ProductsContext = React.createContext()
@@ -36,13 +39,25 @@ export const ProductsProvider = ({ children }) => {
     dispatch({ type: SIDEBAR_CLOSE })
   }
 
-  // get products
+  // fetch all products
   const fetchProducts = async url => {
     dispatch({ type: GET_PRODUCTS_BEGIN })
     try {
       const response = await axios.get(url)
       const products = response.data
       dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products })
+    } catch (error) {
+      dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
+    }
+  }
+
+  // fetch single product
+  const fetchSingleProduct = async url => {
+    dispatch({ type: GET_SINGLE_PRODUCT_BEGIN })
+    try {
+      const response = await axios.get(url)
+      const singleProduct = response.data
+      dispatch({ type: GET_SINGLE_PRODUCT_SUCCESS, payload: singleProduct })
     } catch (error) {
       dispatch({ type: GET_SINGLE_PRODUCT_ERROR })
     }
--- src/reducers/products_reducer.js
+++ src/reducers/products_reducer.js
@@ -6,7 +6,7 @@ import {
   GET_PRODUCTS_ERROR,
   GET_SINGLE_PRODUCT_BEGIN,
   GET_SINGLE_PRODUCT_SUCCESS,
-  GET_SINGLE_PRODUCT_ERROR
+  GET_SINGLE_PRODUCT_ERROR,
 } from "../actions"
 
 // products reducers
@@ -21,11 +21,10 @@ const products_reducer = (state, action) => {
     return { ...state, isSidebarOpen: false }
   }
 
-  // get products
+  // get all products
   if (action.type === GET_PRODUCTS_BEGIN) {
     return { ...state, products_loading: true }
   }
-
   // products accessed successfully
   if (action.type === GET_PRODUCTS_SUCCESS) {
     const featured_products = action.payload.filter(
@@ -35,13 +34,36 @@ const products_reducer = (state, action) => {
       ...state,
       products_loading: false,
       products: action.payload,
-      featured_products
+      featured_products,
     }
   }
 
-  // errors
+  // get single product
+  if (action.type === GET_SINGLE_PRODUCT_BEGIN) {
+    return {
+      ...state,
+      single_product_loading: true,
+      single_product_error: false,
+    }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_SUCCESS) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product: action.payload,
+    }
+  }
+
+  // errors generated
   if (action.type === GET_PRODUCTS_ERROR) {
     return { ...state, products_loading: false, products_error: true }
+  }
+  if (action.type === GET_SINGLE_PRODUCT_ERROR) {
+    return {
+      ...state,
+      single_product_loading: false,
+      single_product_error: true,
+    }
   }
 
   // throw errors
