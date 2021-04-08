a78428c6f50f551ee2af58d02fab80bb1b575b81
Lawrence <locaulker@gmail.com>
2021-04-08 05:13:31 -0400

    constructed grid view layout

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import styled from 'styled-components'
-import { useFilterContext } from '../context/filter_context'
-import { getUniqueValues, formatPrice } from '../utils/helpers'
-import { FaCheck } from 'react-icons/fa'
+import React from "react"
+import styled from "styled-components"
+import { useFilterContext } from "../context/filter_context"
+import { getUniqueValues, formatPrice } from "../utils/helpers"
+import { FaCheck } from "react-icons/fa"
 
 const Filters = () => {
-  return <h4>filters</h4>
+  return <h4>Filters</h4>
 }
 
 const Wrapper = styled.section`
--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import { BsFillGridFill, BsList } from 'react-icons/bs'
-import styled from 'styled-components'
+import React from "react"
+import { useFilterContext } from "../context/filter_context"
+import { BsFillGridFill, BsList } from "react-icons/bs"
+import styled from "styled-components"
 const Sort = () => {
-  return <h4>sort </h4>
+  return <h4>Sort </h4>
 }
 
 const Wrapper = styled.section`
