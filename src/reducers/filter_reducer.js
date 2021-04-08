b73086b09e25619d8606006c7e03e4c1aba7cba1
Lawrence <locaulker@gmail.com>
2021-04-08 01:47:49 -0400

    setup FilterContext

--- src/reducers/filter_reducer.js
+++ src/reducers/filter_reducer.js
@@ -6,11 +6,18 @@ import {
   SORT_PRODUCTS,
   UPDATE_FILTERS,
   FILTER_PRODUCTS,
-  CLEAR_FILTERS,
-} from '../actions'
+  CLEAR_FILTERS
+} from "../actions"
 
 const filter_reducer = (state, action) => {
-  return state
+  if (action.type === LOAD_PRODUCTS) {
+    return {
+      ...state,
+      all_products: [...action.payload],
+      filtered_products: [...action.payload]
+    }
+  }
+
   throw new Error(`No Matching "${action.type}" - action type`)
 }
 
