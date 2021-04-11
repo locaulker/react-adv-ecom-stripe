365106f1fba941c3803bfeb3b708bf8a96faa009
Lawrence O. Caulker <locaulker@gmail.com>
2021-04-11 08:36:14 -0400

    setup cart columns

--- src/components/CartColumns.js
+++ src/components/CartColumns.js
@@ -1,8 +1,19 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react"
+import styled from "styled-components"
 
 const CartColumns = () => {
-  return <h4>cart columns</h4>
+  return (
+    <Wrapper>
+      <div className="content">
+        <h5>item</h5>
+        <h5>price</h5>
+        <h5>quantity</h5>
+        <h5>subtotal</h5>
+        <span></span>
+      </div>
+      <hr />
+    </Wrapper>
+  )
 }
 
 const Wrapper = styled.div`
@@ -16,7 +27,8 @@ const Wrapper = styled.div`
       column-gap: 1rem;
       h5 {
         color: var(--clr-grey-5);
-        font-weight: 400;
+        font-weight: 700;
+        text-transform: capitalize;
       }
     }
 
