a422ee5378472fc36479c5f7f5b4e3fff2a1be18
Lawrence <locaulker@gmail.com>
2021-04-13 06:21:48 -0400

    implemented Auth0 Provider

--- src/index.js
+++ src/index.js
@@ -8,23 +8,32 @@ import { ProductsProvider } from "./context/products_context"
 import { FilterProvider } from "./context/filter_context"
 import { CartProvider } from "./context/cart_context"
 import { UserProvider } from "./context/user_context"
+import { Auth0Provider } from "@auth0/auth0-react"
 
 /**
  * Auth0
  * Domain: dev-penxv1w4.us.auth0.com
  * Client ID: TTa8m4dNDAAPcuJOXI2XSu03lpUpIlEw
+ * Secret: dd1h6S90w_bVLJVOdCP-M6k_Uj2xdSq3-i9jqyhrb1HvfkMUdJXHpuuA_2Fz3UMi
  */
 
 // authentication imports
-import { Auth0Provider } from "@auth0/auth0-react"
+// import { Auth0Provider } from "@auth0/auth0-react"
 
 ReactDOM.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>,
+  <Auth0Provider
+    domain="dev-penxv1w4.us.auth0.com"
+    clientId="TTa8m4dNDAAPcuJOXI2XSu03lpUpIlEw"
+    redirectUri={window.location.origin}
+    cacheLocation="localstorage"
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>,
   document.getElementById("root")
 )
